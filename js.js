f78baa2de714963c381da97e6711553e1df02cc5
lalitchoudhary555 <lalitjhajharia555@gmail.com>
2024-09-01 18:30:29 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,7 @@
+// function showAlert() {
+//     alert('Thank you ');
+//     console.log("button click")
+// }
+function showAlert() {
+    alert('Thank you for your interest! I will get back to you soon.');
+}
